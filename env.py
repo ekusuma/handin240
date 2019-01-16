6c1195c2a34619edd4081108cf8ea9a681e0686c
ekusuma <ekusuma@cmu.edu>
2019-01-16 17:53:51 -0500

    Updated env variables

--- env.py
+++ env.py
@@ -3,11 +3,10 @@
 # Environment variables (such as paths) that are useful for the handin tool. If
 # intended directories do change over time, redefine their values here.
 #
-# This env file is meant for the staff-facing files, and has to be updated with
-# paths relevant for the staff scripts. THIS FILE IS DIFFERENT FROM THE env.py
-# IN THE STUDENT BRANCH.
-#
 # Edric Kusuma (ekusuma)
+
+# Path to repo's folder
+REPO_DIR = "/afs/ece.cmu.edu/class/ee240/STAFF/ekusuma/240handin";
 
 # Main course directory in AFS space
 #COURSE_DIR = "/afs/ece.cmu.edu/class/ee240";
@@ -17,7 +16,7 @@ COURSE_DIR = "/afs/ece.cmu.edu/class/ee240/STAFF/ekusuma/240handin/course";
 # Directory where student submissions should be entered
 HANDIN_DIR = COURSE_DIR + "/handin";
 
-# Directory where homework files will be (currently unused)
+# Directory where homework files will be
 HANDOUT_DIR = COURSE_DIR + "/handout";
 
 # Course staff directory (students do NOT have access to here!)
@@ -27,4 +26,7 @@ STAFF_DIR = COURSE_DIR + "/STAFF";
 ROSTER = STAFF_DIR + "/roster_s19.csv";
 
 # Location of homework cfg files
-CFG_DIR = STAFF_DIR + "/scripts/handinCfgs";
+CFG_DIR = HANDOUT_DIR + "/hwConfigs";
+
+# Location of font files for ReportLab
+FONT_DIR = REPO_DIR + "/lib";
